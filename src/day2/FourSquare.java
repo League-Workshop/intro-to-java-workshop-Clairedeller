7e37ba1dc60d3370698feed83c5a362c34179287
Kadorus <League@AdministorsiMac.attlocal.net>
2018-01-02 14:58:09 -0800

    DAY 1 Worksho[p

--- src/day2/FourSquare.java
+++ src/day2/FourSquare.java
@@ -6,17 +6,20 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot sWAG = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+sWAG.setSpeed(100);
 		// 5. Set the pen width to 5
+sWAG.setPenWidth(5);
 
 		// 6. Do steps #7 to #8 four times...
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
 
@@ -26,6 +29,11 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 
+		for (int i = 0; i < 4; i++) {
+			
+		
+		sWAG.move(150);
+		sWAG.turn(90);
 		
 		
 	}
--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -3,13 +3,28 @@ package day2;
 import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
 		// START HERE
+	System.out.println("Hello World!!!");
+	Robot sWAG = new Robot();
+	sWAG.sparkle();
+	sWAG.penDown();
+	sWAG.setRandomPenColor();
+	sWAG.setSpeed(90);
 	
+	
+	for (int i = 0; i < 4; i++) {
 		
+	
+	sWAG.move(150);
+	sWAG.turn(90);
+
+	}
 		
 	}
 }
